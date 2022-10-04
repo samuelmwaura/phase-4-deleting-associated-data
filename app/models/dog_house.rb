ca57adb03e5b83a88adde2af8d95cfe6ce3e51df
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-10-05 00:02:21 +0300

    Completed the lab

--- app/models/dog_house.rb
+++ app/models/dog_house.rb
@@ -1,3 +1,7 @@
 class DogHouse < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy #This is the line that cascades the delete from this model to the associated reviews
+  #This behaviour can also be achieved in a more verbose manner i.e first fetch all the associated reviews
+  #DogHouse.first.reviews.destroy_all
+  #DogHouse.first.destroy
+  #The dependent: :delete does the same by calling internally defines methods.It aoutomatically removes associated record when the parent record is deleted.
 end
